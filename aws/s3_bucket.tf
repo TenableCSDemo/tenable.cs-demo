7cd4fd4ad1cb8a48e9e219fa7aa2cead56915ef9
Tenable.cs <admin@accurics.com>
2023-02-12 07:23:38 -0600

    Auto Remediation By Tenable.cs

--- aws/s3_bucket.tf
+++ aws/s3_bucket.tf
@@ -2,4 +2,9 @@ resource "aws_s3_bucket" "tenable_cs_demo_s3_bucket" {
   bucket = "tenablecsdemos3bucket"
   acl    = "public-read"
   tags   = var.default_tags
+
+  versioning {
+    enabled    = true
+    mfa_delete = true
+  }
 }
